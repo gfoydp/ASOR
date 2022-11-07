27ea56cdab4e9010de430f3bb1d2450466161a7d
gfoydp <gfoydp@gmail.com>
2022-11-07 12:17:48 +0100

    ej 14

---
+++ ej14.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <errno.h>
+#include <time.h>
+
+
+int main() {
+
+    time_t t;
+    struct tm *y;
+
+    t = time(&t);
+
+    if((y=localtime(&t)) == NULL){
+        perror("Error: ");
+        return -1;
+    }
+
+    y->tm_year;
+
+    printf("El año es %d \n",y->tm_year + 1900);
+
+
+   return 1;
+}
+
