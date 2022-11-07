8e0797b86458dea9829dc3aab87f526e0b594fdb
gfoydp <gfoydp@gmail.com>
2022-11-07 13:49:46 +0100

    perror de ej7

--- ej7.c
+++ ej7.c
@@ -6,9 +6,24 @@
 
 int main() {
 
-    printf("Numero maximo de argumentos: %ld \n", pathconf("/home/cursoredes/Desktop/P1/ASOR/",_PC_LINK_MAX));
-    printf("Numero maximo de hijos: %ld \n", pathconf("/home/cursoredes/Desktop/P1/ASOR/",_PC_PATH_MAX));
-    printf("Numero maximo de archivos abiertos: %ld \n", pathconf("/home/cursoredes/Desktop/P1/ASOR/",_PC_NAME_MAX));
+    long link,path, name;
+
+    if((link = pathconf("/home/cursoredes/Desktop/P1/ASOR/",_PC_LINK_MAX)) == -1){
+        perror("Error al obtener el numero maximo de enlaces");
+        return -1;
+    }
+    if((path = pathconf("/home/cursoredes/Desktop/P1/ASOR/",_PC_PATH_MAX)) == -1){
+        perror("Error al obtener el tamaño maximo de ruta");
+        return -1;
+    }
+    if((name = pathconf("/home/cursoredes/Desktop/P1/ASOR/",_PC_NAME_MAX)) == -1){
+        perror("Error al obtener el tamaño maximo de nombre");
+        return -1;
+    }
+
+    printf("Numero maximo de enlaces: %ld \n", link);
+    printf("Tamaño maximo de ruta: %ld \n", path );
+    printf("Tamaño maximo de nombre: %ld \n", name);
 
    return 1;
 }
