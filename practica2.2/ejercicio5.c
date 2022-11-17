ce3566606d366a3e4570dcfd2f7bcdd4f5c2cf83
gfoydp <gfoydp@gmail.com>
2022-11-17 14:39:48 +0100

    ejercicio5

---
+++ practica2.2/ejercicio5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+
+
+
+int main() {
+   int fd;
+   fd = open("prueba.txt", O_RDWR | O_CREAT,00645);
+
+   if(fd == -1){
+    perror("Error al abrir o crear fichero: ");
+    return -1;
+   }
+    return 0;
+   
+}
+
